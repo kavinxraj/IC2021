4f523826daa40ed32f400a12f5309f8fa481434c
dskavinraj <dskavinraj@gmail.com>
2022-04-15 09:31:50 +0530

    adding async fifo #style 2 from Clifford E. Cummings : Sunbrust Publications

---
+++ filelist/async_fifo/fifo2_rtl.f
@@ -0,0 +1,6 @@
+${MODEL_ROOT}/src/rtl/async_fifo/fifomem2.v
+${MODEL_ROOT}/src/rtl/async_fifo/rptr_empty_2.v
+${MODEL_ROOT}/src/rtl/async_fifo/wptr_full_2.v
+${MODEL_ROOT}/src/rtl/async_fifo/async_cmp2.v
+${MODEL_ROOT}/src/rtl/async_fifo/fifo2.v
+
